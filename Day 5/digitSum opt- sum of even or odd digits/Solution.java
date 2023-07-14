1e09907b976355dca47c2f238ef43ef9495634c1
Ramkishor.S <96153571+ram05kishor12@users.noreply.github.com>
2023-07-14 13:48:37 +0530

    Add files via upload

---
+++ Day 5/digitSum opt- sum of even or odd digits/Solution.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+// Read only region start
+class UserMainCode
+{
+    public int Solution(int input1) {
+        // Read only region end
+        // Write code here...
+        
+
+        // throw new UnsupportedOperationException("IsEven(int input1)");
+    }
+}
